308e4694ad1de588c5069492d99d265b08a5afdd
SamLin <1428293926@qq.com>
2024-04-17 00:13:57 +0800

    1

---
+++ src/test/java/samlin/first/bigevent/BigEventApplicationTests.java
@@ -0,0 +1,13 @@
+package samlin.first.bigevent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BigEventApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
